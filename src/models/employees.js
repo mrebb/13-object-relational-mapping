f5b5d6c33194413eddc7987c747b517747c63570
Mrebb <madhu.rebbana@gmail.com>
2018-06-21 18:32:01 -0700

    env mongodb URI set

--- src/models/employees.js
+++ src/models/employees.js
@@ -1,8 +1,7 @@
 'use strict';
 import mongoose from 'mongoose';
 require('dotenv').config();
-let MONGODB_URI = 'mongodb://heroku_270zxqhk:6drl87pgjm2pv8968i9nhhmp90@ds015584.mlab.com:15584/heroku_270zxqhk';
-mongoose.connect(MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI);
 const employeeSchema = mongoose.Schema({
   id: {type:String, required: true},
   name: {type: String, required: true},
